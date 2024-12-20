b0be7bfd3b0ffec7450d4db1c8e974c777d81587
Suyash K. <suyu162001@gmail.com>
2024-12-20 22:49:35 +0530

    fix: UI fixes in feed page

--- frontend/src/components/post/Post.vue
+++ frontend/src/components/post/Post.vue
@@ -58,7 +58,7 @@ function comment() {
             <img class="w-full h-full object-cover cursor-pointer" :src="props.content" alt="Content">
             <div class="absolute bottom-0 left-0 w-full h-36 bg-[linear-gradient(0deg,_rgba(0,_0,_0)_0%,_rgba(0,_0,_0,_0)_100%)]"></div>
         </div>
-        <caption class="font-[Oswald] font-[300] text-lg absolute bottom-[7em] sm:bottom-[4em] left-4 text-white">{{ post.body }}</caption>
+        <caption class="font-[Oswald] font-[300] text-lg text-ellipsis overflow-hidden text-nowrap w-11/12 text-left absolute bottom-[7em] sm:bottom-[4em] left-4 text-white">{{ post.body }}</caption>
         <RouterLink :to="{name: 'profile', params: { id: post.created_by.id}}">
             <div class="absolute bottom-[4em] sm:bottom-4 left-4 bg-white p-2 min-w-[10em] max-w-full flex items-center gap-4 rounded-lg">
                 <Avatar width="2em" :src="profilePicture"></Avatar>
