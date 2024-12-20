0d4d1833de53613af297d1f9710826f00512d88b
Suyash K. <suyu162001@gmail.com>
2024-12-20 22:22:35 +0530

    chore: update settings.py for deployment no staticfiles

--- backend/settings.py
+++ backend/settings.py
@@ -157,6 +157,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
