57dd88e610cb990db026bb537c23227cfeec4e13
Suyash K. <suyu162001@gmail.com>
2024-12-20 22:14:54 +0530

    chore: update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
-python manage.py makemigrations
+python3 manage.py makemigrations
 python3 manage.py migrate
